32a1e6b1a57f29bc41471154aeb34f74680ef7ee
KhurasMak <makhurakgotlello@gmail.com>
2023-09-07 12:21:35 -0700

    Printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main that piece art
+ *
+ * Return: 0 when it completes success
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
