8405d0cf5dbd49c4eeef6dba060b197e18339c8a
KhurasMak <makhurakgotlello@gmail.com>
2023-09-08 17:51:25 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - the main main function
+ *
+ * Return: always ways 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+		printf("%d", i);
+	printf("\n");
+	return (0);
+}
