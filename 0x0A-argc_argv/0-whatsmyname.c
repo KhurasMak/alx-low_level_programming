871c9db412ec465938fbcbaee9e6f56c2ad7b811
KhurasMak <makhurakgotlello@gmail.com>
2023-11-22 09:38:36 -0800

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *main - print name of programme
+ *@argc: count via argument
+ *@argv: vector via argument
+ *Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
