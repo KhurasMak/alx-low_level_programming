9aa3f84709f0f122442c82ab32cab55c49e6e442
KhurasMak <makhurakgotlello@gmail.com>
2023-12-14 21:32:06 +0200

    print_bi

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -10,12 +10,12 @@ void print_bi(unsigned long int n)
 		return;
 	print_bi(n >> 1);
 	if ((n & 1) == 1)
-		_putchar(1);
+		_putchar('1');
 	if ((n & 1) == 0)
 		_putchar('0');
 }
 /**
- * print_binary - prints out binar
+ * print_binary - prints out binary
  * @n: decimal to be converted
  */
 void print_binary(unsigned long int n)
